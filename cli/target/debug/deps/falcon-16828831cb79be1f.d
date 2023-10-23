1344df13656914b57028f2136d413db716e97449
yuangwei <yuang.wya@gmail.com>
2023-10-23 15:51:53 +0800

    feat: ✨ Change cli lang ts to rust

--- cli/target/debug/deps/falcon-16828831cb79be1f.d
+++
@@ -1,5 +0,0 @@
-/Users/weiyuang/Desktop/codespace/yuangwei/creative-toolbox/cli/target/debug/deps/falcon-16828831cb79be1f.rmeta: src/main.rs
-
-/Users/weiyuang/Desktop/codespace/yuangwei/creative-toolbox/cli/target/debug/deps/falcon-16828831cb79be1f.d: src/main.rs
-
-src/main.rs:
--- cli/target/debug/deps/falcon-3b77877ef5ed9e2e.d
+++
@@ -1,5 +0,0 @@
-/Users/weiyuang/Desktop/codespace/yuangwei/creative-toolbox/cli/target/debug/deps/falcon-3b77877ef5ed9e2e.rmeta: src/main.rs
-
-/Users/weiyuang/Desktop/codespace/yuangwei/creative-toolbox/cli/target/debug/deps/falcon-3b77877ef5ed9e2e.d: src/main.rs
-
-src/main.rs:
