46d40f561fb30f0c5ade06ac1f08492084c74b6f
yuangwei <yuang.wya@gmail.com>
2024-10-18 10:41:57 +0800

    Base arch

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+  useHead({
+    title: 'Online Clock',
+    meta: [
+      { name: 'description', content: 'A simple online clock with countdown and timer.' }
+    ],
+  })
+</script>
